522ca62a033605d1ea4f14d9e2f9cd370c14b1be
Angel Ivanov <angel.iwanov@gmail.com>
2024-05-30 16:25:13 +0300

    uibuild

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
 
+const Note = require("./models/note");
+
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(cors());
 app.use(morgan("tiny"));
-app.use(express.static("dist"));
 
 let notes = [
   { id: 1, content: "HTML is easy", important: true },
@@ -22,33 +25,50 @@ app.get("/", (request, resposne) => {
   resposne.send("<h1>Hello World!</h1>");
 });
 
-app.get("/api/notes", (request, response) => {
+app.get("/api/notes", async (request, response) => {
+  const notes = await Note.find({});
   response.json(notes);
 });
 
-app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-  if (note) {
-    response.json(note);
-  } else {
-    response.statusMessage = "We couldn't find a note with that id";
-    response.status(404).end();
+app.get("/api/notes/:id", async (request, response, next) => {
+  try {
+    const note = await Note.findById(request.params.id);
+    if (note) {
+      response.json(note);
+    } else {
+      response.status(404).end();
+    }
+  } catch (error) {
+    next(error);
   }
 });
 
-app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
-
-  response.statusMessage = "Note successfully deleted";
-  response.send(204).end();
+app.delete("/api/notes/:id", async (request, response, next) => {
+  try {
+    await Note.findByIdAndDelete(request.params.id);
+    response.status(204).end();
+  } catch (error) {
+    next(error);
+  }
 });
 
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
+app.put("/api/notes/:id", async (request, response, next) => {
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  try {
+    const updatedNote = await Note.findByIdAndUpdate(request.params.id, note, {
+      new: true,
+    });
+    response.json(updatedNote);
+  } catch (error) {
+    return next(error);
+  }
+});
 
 app.post("/api/notes", (request, response) => {
   const body = request.body;
@@ -57,18 +77,35 @@ app.post("/api/notes", (request, response) => {
     return response.status(400).json({ error: "content missing" });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: Boolean(body.important) || false,
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  note.save().then((savedNoted) => {
+    response.json(savedNoted);
+  });
 });
 
-const PORT = process.env.PORT || 3001;
+const unknownEndpoint = (request, response) => {
+  response.status(404).send(`<h1>Unknown route</h1>`);
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
